9e95b5e28f02288e921ea259065bc48b7d0d2ccb
Flatiron School <flatironschool@Flatirons-MacBook-Air-2.local>
2018-10-31 11:16:53 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,37 @@ const codes = [
   "a"
 ];
 
-function init() {
-  // your code here
+
+// const uniqCodes = [
+//   "ArrowUp",
+//   "ArrowDown",
+//   "ArrowLeft",
+//   "ArrowRight",
+//   "b",
+//   "a"
+// ];
+
+let listenLast10 = []
+
+
+function updateListenLast10 (v) {
+  listenLast10.push(v)
+  if (listenLast10.length == 11) {
+    listenLast10.shift()
+    // console.log(listenLast10 + codes)
+  }
+  if (listenLast10.join(',') == codes.join(',')) {
+    alert('code entered')
+  }
 }
+
+
+function init() {
+  document.body.addEventListener("keydown", function(e) {
+  // console.log(e.key)
+  updateListenLast10(e.key)
+})
+}
+
+// console.log("Hello world!");
+// init()
